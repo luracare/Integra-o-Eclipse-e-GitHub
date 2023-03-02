2fb699d750e3c6695976ec6651df69f8a03f782a
Lucca Rafael <104457141+luracare@users.noreply.github.com>
2023-03-02 18:49:37 -0300

    Add files via upload

---
+++ ex1.java
@@ -0,0 +1,13 @@
+package ex1;
+import java.util.Scanner;
+public class ex1 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Digite 2 números");
+		int x=sc.nextInt(),y=sc.nextInt();
+		System.out.println(x+y);
+		sc.close();
+	}
+
+}
